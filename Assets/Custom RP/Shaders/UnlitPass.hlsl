fabcc164709aafc0d8335b9686aadbe44573ee53
hmelnoyhmel <bulaevap97@gmail.com>
2025-01-11 19:16:13 +0300

    Added custom shader, pass and utility shader files. Minor restructurization

---
+++ Assets/Custom RP/Shaders/UnlitPass.hlsl
@@ -0,0 +1,19 @@
+#ifndef CUSTOM_UNLIT_PASS_INCLUDED
+#define CUSTOM_UNLIT_PASS_INCLUDED
+
+#include "../ShaderLibrary/Common.hlsl"
+
+// The main job of the vertex function is to convert the original vertex position to the correct space.
+float4 UnlitPassVertex(float3 positionOS : POSITION) : SV_POSITION
+{
+    const float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+    return TransformWorldToHClip(positionWS);
+}
+
+float4 UnlitPassFragment() : SV_TARGET
+{
+    return 0.0f;
+}
+
+#endif
+
